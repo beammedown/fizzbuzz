a873bcde6ae59e68aca1e4a36b89555d2d908e86
beammedown <trongero007@gmail.com>
2023-03-26 22:15:46 +0200

    added unreadable

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     for i in 0..100  {
-        if i%5 == 0 && i%3 == 0 {
+        if i%15 == 0 {
             println!("Fizzbuzz");
         } else if i%5 == 0 {
             println!("Buzz");
@@ -10,4 +10,9 @@ fn main() {
             println!("{}", i);
         }
     }
+    unreadable()
+}
+
+fn unreadable() {
+    for i in 0..100 {if i%15 == 0 {println!("Fizzbuzz")}else if i%5==0{println!("Buzz")}else if i%3==0{println!("Fizz")}else{println!("{}", i)}}
 }
