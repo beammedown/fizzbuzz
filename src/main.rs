d95f195cd3bb7b12893df6f84c374cae98263b88
Gaston Liepach <77543941+beammedown@users.noreply.github.com>
2023-09-07 18:58:34 +0000

    trivial performance comparison match vs if

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
+use std::time::{Duration, Instant};
+use std::thread;
+
 fn main() {
-    for i in 0..100  {
+    let st1 = Instant::now();
+    ifniceformatted();
+    let ft1 = st1.elapsed();
+    let st2 = Instant::now();
+    unreadable();
+    let ft2 = st2.elapsed();
+    let st3 = Instant::now();
+    matchmaker();
+    let ft3 = st3.elapsed();
+    thread::sleep(Duration::from_secs(1));
+    println!("Mode one: {} \n Mode two: {} \n Mode three: {}", Duration::as_micros(&ft1), Duration::as_micros(&ft2), Duration::as_micros(&ft3));
+}
+
+fn ifniceformatted() {
+    for i in 0..1001  {
         if i%15 == 0 {
             println!("Fizzbuzz");
         } else if i%5 == 0 {
@@ -10,9 +27,19 @@ fn main() {
             println!("{}", i);
         }
     }
-    unreadable()
 }
 
 fn unreadable() {
-    for i in 0..100 {if i%15 == 0 {println!("Fizzbuzz")}else if i%5==0{println!("Buzz")}else if i%3==0{println!("Fizz")}else{println!("{}", i)}}
+    for i in 0..1001 {if i%15 == 0 {println!("Fizzbuzz")}else if i%5==0{println!("Buzz")}else if i%3==0{println!("Fizz")}else{println!("{}", i)}}
+}
+
+fn matchmaker() {
+    for i in 0..1001 {
+        match (i%3, i%5) {
+            (0, 0) => println!("Fizzbuzz"),
+            (0, _) => println!("Fizz"),
+            (_, 0) => println!("Buzz"),
+            (_, _) => println!("{}", i)
+        }
+    }
 }
